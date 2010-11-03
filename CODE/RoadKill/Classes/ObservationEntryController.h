07e758a00bd93d83c0bbab02ad9c65ff175ad43e
Gerard Hickey <hickey@kinetic-compute.com>
2010-11-03 13:11:43 -0700

    Added ObservationEntryController with XIB to manage observation creation.

---
+++ CODE/RoadKill/Classes/ObservationEntryController.h
@@ -0,0 +1,20 @@
+//
+//  ObservationEntryController.h
+//  RoadKill
+//
+//  Created by Gerard Hickey on 11/2/10.
+//  Copyright 2010 Seattle RoadKill Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ObservationEntryController : UITableViewController {
+
+}
+
+
+//- (void) doDone:(id)sender;
+
+
+@end
