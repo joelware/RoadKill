4dfef673e821681085758df0363b618b08864745
Pamela Arnone <parnone@me.com>
2010-11-09 19:22:45 -0800

    Added Photo class and updated the Observation class to reflect that. Added code to manage using UIImage in the data model.

--- CODE/RoadKill/Classes/SpeciesSelectionVC.m
+++ CODE/RoadKill/Classes/SpeciesSelectionVC.m
@@ -163,21 +163,7 @@
 
 #pragma mark -
 #pragma mark Table view delegate
-#if 0
-	//TODO: need to do this next
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-		// Navigation logic may go here. Create and push another view controller.
-	/*
-	 <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-	 [self.navigationController pushViewController:detailViewController animated:YES];
-	 [detailViewController release];
-	 */
-}
-#endif
 
-#if 1
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath 
 {		
 		//http://stackoverflow.com/questions/974170/uitableview-having-problems-changing-accessory-when-selected
@@ -222,7 +208,6 @@
 	}	
 	[newViewController release];
 }
-#endif
 
 #pragma mark -
 #pragma mark Fetched results controller
